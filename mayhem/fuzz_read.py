fa3cefa2c861a62a7ac783b6a7cea535af7d9cb0
Bailey Capuano <bcapuano@asu.edu>
2022-12-21 15:30:31 -0500

    updated fuzzer

--- mayhem/fuzz_read.py
+++ mayhem/fuzz_read.py
@@ -2,18 +2,30 @@
 
 import atheris
 import sys
+import io
+from contextlib import contextmanager
 import fuzz_helpers
 
-with atheris.instrument_imports(include=['environs']):
+with atheris.instrument_imports():
     import environs
 
+@contextmanager
+def nostdout():
+    save_stdout = sys.stdout
+    save_stderr = sys.stderr
+    sys.stdout = io.StringIO()
+    sys.stderr = io.StringIO()
+    yield
+    sys.stdout = save_stdout
+    sys.stderr = save_stderr
 
 def TestOneInput(data):
     fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
-    with fdp.ConsumeTemporaryFile('.env', all_data=True, as_bytes=False) as fname:
+    with fdp.ConsumeTemporaryFile('.env', all_data=True, as_bytes=False) as fname, nostdout():
         env = environs.Env()
         env.read_env(fname)
         env.dump()
+        env.seal()
 
 
 def main():
